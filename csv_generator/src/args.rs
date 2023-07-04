620ed67ede90cecc29c5ec27536a380750e013e9
kevin <hyunbin7303@gmail.com>
2023-07-04 08:13:42 -0400

    #1

--- csv_generator/src/args.rs
+++ csv_generator/src/args.rs
@@ -1,3 +1,60 @@
 use clap:: {
     Args, Parser, Subcommand
+};
+// use std::error::Error;
+
+#[derive(Debug,Parser)]
+#[clap(author,version,about, long_about = None)]
+pub struct CliArgs {
+    #[clap(subcommand)]
+    pub userinput : EntityType,
+    pub how_many_lines : usize
+}
+
+#[derive(Debug, Subcommand)]
+pub enum EntityType {
+
+    /// Set up cols and rows manually. 
+    Custom(CustomCommand),
+
+    /// Using Template that already existing.
+    Template(TemplateCommand),
+
+    /// Randomly Generate (Default 5 cols, 10 rows)
+    AutoGenerate(AutoGenerateCommand),
+}
+
+
+
+#[derive(Debug, Args)]
+pub struct CustomCommand {
+    #[clap(subcommand)]
+    pub command : CustomSubCommand,
+
+}
+#[derive(Debug, Args)]
+pub struct TemplateCommand {
+    #[clap(subcommand)]
+    pub command : BuiltInSubCommand,
+}
+#[derive(Debug, Args)]
+pub struct AutoGenerateCommand {
+    #[clap(subcommand)]
+    pub command : BuiltInSubCommand,
+}
+
+#[derive(Debug,Subcommand)]
+pub enum BuiltInSubCommand {
+    Create(CreateFile),
+}
+
+#[derive(Debug,Subcommand)]
+pub enum CustomSubCommand {
+    Create(CreateFile),
+}
+#[derive(Debug,Args)]
+pub struct CreateFile {
+    pub filename : String,
+    pub cols : i64,
+    // pub rows : i64, 
 }
--- csv_generator/src/main.rs
+++ csv_generator/src/main.rs
@@ -1,64 +1,46 @@
 use clap::Parser;
 use std::error::Error;
-
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
-}
-
+mod args;
 
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args = Args::parse();
+    let args: args::CliArgs = args::CliArgs::parse(); 
+    println!("{:?}", args);
+    println!("{}", args.how_many_lines);
+   // match &arg.command {
+    // Commands::Caption(options) => {
+        // generate_caption(options)
+    //}
+   //}
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    
+    // Get how many 
+
+    Ok(())
+}
+pub fn read_input_csv_file() -> Result<(), Box<dyn Error>> {
+    // Reader from path of test csv file
+    let reader = csv::Reader::from_path("sample_internet.csv");
+
+    // Iterate through each record (line) in the CSV
+    for record in reader?.records() {
+
+        let string_record = record?;
+
+        // Print the string record as an object
+        println!("{:?}", string_record);
     }
     Ok(())
-
-    // Getting user input 
-
-     
 }
-// fn main() -> Result<(), Box<dyn Error>> {
 
-//     let args = Cli::parse();
-
-// }
-// pub fn read_input_csv_file() -> Result<(), Box<dyn Error>> {
-//     // Reader from path of test csv file
-//     let reader = csv::Reader::from_path("sample_internet.csv");
-
-//     // Iterate through each record (line) in the CSV
-//     for record in reader?.records() {
-
-//         let string_record = record?;
-
-//         // Print the string record as an object
-//         println!("{:?}", string_record);
-//     }
-//     Ok(())
-// }
-
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
-    fn test_add() {
-        assert_eq!(add(1, 2), 3);
+    fn read_input_csv_file_should_returnOk()
+    {
+    //     assert_eq!(add(1, 2), 3);
     }
 }
